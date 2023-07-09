7ae215b39f21967e309f750b9e2b846a3a8c5580
Akram Ali <akram.exp02@gmail.com>
2023-07-09 19:04:26 +0530

    Display random quotes in home page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quoteContainer = document.getElementById("quote-generator");
 const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("quote-author");
+const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 
@@ -10,19 +10,24 @@ let apiQuotes = [];
 function newQuote() {
   // to pick a random quote from array of quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-//   check if the author field is empty and replace it with unknown
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-  
+  //   check if the author field is empty and replace it with unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //   check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
   quoteText.textContent = quote.text;
 }
 
 // Get quotes from api
 async function getQuotes() {
-  const apiUrl = "";
+  const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
