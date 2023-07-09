3a82d83a844f0838dece9f85bbe70e2a1f85bdf2
Akram Ali <akram.exp02@gmail.com>
2023-07-09 20:04:45 +0530

    Add loader

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById("quote-generator");
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+  loading();
   // to pick a random quote from array of quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //   check if the author field is empty and replace it with unknown
@@ -22,7 +36,9 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  //   set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from api
@@ -40,12 +56,12 @@ async function getQuotes() {
 // tweet quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, '_blank');
+  window.open(twitterUrl, "_blank");
 }
 
 // event listener
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // on load
 getQuotes();
