58625e14c3b77e7b7837511a883a91be5013bb57
Akram Ali <akram.exp02@gmail.com>
2023-07-09 18:52:00 +0530

    check if the author of quote exits

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+const quoteContainer = document.getElementById("quote-generator");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("quote-author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
+
+// show new quote
+function newQuote() {
+  // to pick a random quote from array of quote
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//   check if the author field is empty and replace it with unknown
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+  
+  quoteText.textContent = quote.text;
+}
 
 // Get quotes from api
 async function getQuotes() {
-    const apiUrl = ""
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes)
-    } catch (error) {
-        // catch error here
-
-    }
+  const apiUrl = "";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // catch error here
+  }
 }
 
 // on load
